a1e04c2e99d7e6a9ff3d8f05cb5dd47243d517ff
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-12-09 10:45:10 +0200

    update new winning paths.

--- pages/index.js
+++ pages/index.js
@@ -80,6 +80,14 @@ export default function Home() {
     "r1c3-r2c3-r3c3-",
     "r1c1-r2c2-r3c3-",
     "r1c3-r2c2-r3c1-",
+    "r1c3-r1c2-r1c1-",
+    "r2c3-r2c2-r2c1-",
+    "r3c3-r3c2-r3c1-",
+    "r3c1-r2c1-r1c1-",
+    "r3c2-r2c2-r1c2-",
+    "r3c3-r2c3-r1c3-",
+    "r3c3-r2c2-r1c1-",
+    "r3c1-r2c2-r1c3-",
   ];
 
   const playBlock = (blockNum, block, location) => {
